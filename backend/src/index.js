f6ddaa4a551aa1cd5784902dd9e994e68f9605ad
Goldigger-1 <Goldigger666@proton.me>
2025-04-10 00:55:20 +0200

    s

--- backend/src/index.js
+++ backend/src/index.js
@@ -60,6 +60,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
